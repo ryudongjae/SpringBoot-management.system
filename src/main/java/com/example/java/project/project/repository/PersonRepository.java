8792b6a0dd1f1c94acb8eceeda81791f3887270c
ryudongjae <rdj10149@gmail.com>
2021-01-13 22:06:50 +0900

    .

--- src/main/java/com/example/java/project/project/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.java.project.project.repository;
-
-import com.example.java.project.project.domain.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
